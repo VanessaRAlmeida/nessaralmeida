e25e8284d34413100e7a762bceed367d6001cab6
VanessaRAlmeida <115666855+VanessaRAlmeida@users.noreply.github.com>
2025-06-22 18:57:14 -0300

    Add files via upload

---
+++ src/test/java/com/example/ExemploTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExemploTest {
+
+    @Test
+    public void testSomaSimples() {
+        assertEquals(2, 1 + 1);
+    }
+}
